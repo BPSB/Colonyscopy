dc5680f7b42144dd7f6749601ff3407944f14b51
Gerrit Ansmann <bla@example.com>
2018-07-17 18:56:49 +0200

    Docstrings and skeleton of segmenting function.

--- colonyscopy/_colonyscopy.py
+++ colonyscopy/_colonyscopy.py
@@ -1,7 +1,25 @@
 
 
 class ColonyArray(object):
-	def __init__(array,dimensions=None):
+	"""
+		The basic class for handling colony arrays.
+		
+		Parameters
+		----------
+		array : NumPy array
+			An three-dimensional array containing all the raw data.
+			The first two dimensions are spacial, the third dimension is time.
+		
+		dimensions : pair of integers
+			The number of colonies in each direction of the colony array.
+	"""
+	def __init__(array,dimensions=(12,8)):
 		self.array = array
-		self.format = dimensions
+		self.format = np.asarray(dimensions,dtype=int)
+	
+	def segment(self):
+		"""
+		Tries to automatically segment the images into individual colonies.
+		"""
+		pass
 
