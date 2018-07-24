375badd4e6813a8a2452ac13ae25588853a7b4c2
Gerrit Ansmann <bla@example.com>
2018-07-24 17:50:08 +0200

    First working segmenting.

--- colonyscopy/_colonyscopy.py
+++ colonyscopy/_colonyscopy.py
@@ -1,4 +1,26 @@
 import numpy as np
+from scipy.signal import argrelmax
+from colonyscopy.tools import smoothen
+
+class ColonyscopyFailedHeuristic(Exception):
+	pass
+
+class Colony(object):
+	"""
+		Class representing a single colony array.
+		
+		Parameters
+		----------
+		images : NumPy array
+			A four-dimensional array containing the data for the actual colony. This can be a slice of a bigger array. The first dimension is time; the next two dimensions are spacial; the last dimension has length 3 and is colour.
+		
+		centre : pair of integers
+			The centre of the colony in the coordinates of `images`.
+	"""
+	
+	def __init__(self,images,centre):
+		self.images = images
+		self.centre = np.asarray(centre,dtype=int)
 
 class ColonyArray(object):
 	"""
@@ -6,20 +28,86 @@ class ColonyArray(object):
 		
 		Parameters
 		----------
-		array : NumPy array
+		images : NumPy array
 			A four-dimensional array containing all the raw data.
 			The first dimension is time; the next two dimensions are spacial; the last dimension has length 3 and is colour.
 		
 		dimensions : pair of integers
 			The number of colonies in each direction of the colony array.
 	"""
-	def __init__(self,array,dimensions=(12,8)):
-		self.array = array
-		self.format = np.asarray(dimensions,dtype=int)
+	
+	def __init__(self,images,dimensions=(12,8)):
+		self.images = images
+		self.dimensions = np.asarray(dimensions,dtype=int)
+		self.background = np.average(images,axis=(0,1,2))
+		self.resolution = images.shape[1:3]
+	
+	@property
+	def temp_mean(self):
+		if not hasattr(self,"_temp_mean"):
+			self._temp_mean = np.average(self.images,axis=0)
+		return self._temp_mean
 	
 	def segment(self):
 		"""
 		Tries to automatically segment the images into individual colonies.
 		"""
-		pass
+		intensities = np.abs(np.sum(self.temp_mean-self.background,axis=2))
+		profiles = [np.average(intensities,axis=i) for i in (1,0)]
+		
+		for smooth_width in range(1,int(self.resolution[0]/self.dimensions[0])):
+			smoothed_profiles = [smoothen(profiles[i],smooth_width) for i in (0,1)]
+			self._coordinates = [argrelmax(smoothed_profiles[i])[0] for i in (0,1)]
+			if all(
+					len(self._coordinates[i]) == self.dimensions[i]
+					for i in (0,1)
+				):
+				break
+		else:
+			raise ColonyscopyFailedHeuristic("Could not detect colony coordinates. ")
+		# for smooth_width in range(1,int(self.resolution[0]/self.dimensions[0])):
+		# 	self._coordinates = [None,None]
+		# 	for i in (0,1):
+		# 		smoothened_profile = smoothen(profiles[i],smooth_width)
+		# 		self._coordinates[i] = argrelmax(smoothened_profile)[0]
+		# 		if len(self._coordinates[i]) != self.dimensions[i]:
+		# 			# Continue outer loop:
+		# 			print(self._coordinates)
+		# 			break
+		# 	else:
+		# 		# If inner loop was not broken (i.e., the number of maxima and the format match), break outer loop:
+		# 		break
+		# else:
+		# 	# If outer loop was not broken:
+		# 	raise ColonyscopyFailedHeuristic("Could not detect colony coordinates. ")
+	
+	@property
+	def coordinates(self):
+		"""
+		Returns a pair of one-dimensional arrays containing for each direction the coordinates where colonies are placed.
+		"""
+		if not hasattr(self,"_coordinates"):
+			self.segment()
+		return self._coordinates
+
+	@property
+	def centres(self):
+		"""
+		Returns a three dimensional array, containing the centres of the colonies by two-dimensional index.
+		"""
+		return np.dstack(np.meshgrid(*self.coordinates,indexing="ij"))
+
+	@property
+	def borders(self):
+		"""
+		Returns a pair of one-dimensional arrays containing for each direction the positions of borders of colonies (including 0 and the image size).
+		"""
+		return [
+				np.hstack((
+					0,
+					(self.coordinates[i][1:]+self.coordinates[i][:-1])/2,
+					self.resolution[i]))
+				for i in (0,1)
+			]
+
 
--- tests/test_segmenting.py
+++ tests/test_segmenting.py
@@ -1,27 +1,32 @@
-from pytest import raises
+from pytest import raises, approx
 import numpy as np
+from numpy.testing import assert_allclose
 from colonyscopy import ColonyArray
 from generate_data import generate_data,colony_centres
 
-times = 10
-dimensions = (12,8)
-colony_sizes = np.random.uniform(10,100,(times,*dimensions))
-resolution = (900,600)
-bg = np.random.randint(0,256,3)
-fg = np.random.randint(0,256,3)
+def test_segmenting():
+	times = 10
+	dimensions = (12,8)
+	colony_sizes = np.random.uniform(10,20,(times,*dimensions))
+	resolution = (900,600)
+	bg = np.random.randint(0,256,3)
+	fg = np.random.randint(0,256,3)
+	
+	some_array = ColonyArray(
+			images = generate_data(colony_sizes,resolution,bg,fg),
+			dimensions = dimensions
+		)
+	
+	centres_control = colony_centres(dimensions,resolution)
+	assert_allclose(some_array.centres,centres_control,atol=1)
+	
+	for i in (0,1):
+		borders_control = np.linspace(0,resolution[i],dimensions[i]+1)
+		assert_allclose(some_array.borders[i],borders_control,atol=1)
+	
+	# for i,j in [ (dimensions[0],0), (0,dimensions[1]) ]:
+	# 	with raises(IndexError):
+	# 		some_array[i,j]
 
-some_array = ColonyArray(
-		array = generate_data(colony_sizes,resolution,bg,fg),
-		dimensions = dimensions
-	)
 
-centres = colony_centres(dimensions,resolution)
-
-for i in range(dimensions[0]):
-	for j in range(dimensions[1]):
-		assert centres[i,j] == some_array[i,j].centre
-
-for i,j in [ (dimensions[0],0), (0,dimensions[1]) ]:
-	with raises(IndexError):
-		centres[i,j]
 
