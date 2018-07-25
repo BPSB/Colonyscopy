605bac1cf6f3c2197e1b848e15533d95064fa535
Gerrit Ansmann <bla@example.com>
2018-07-25 09:25:32 +0200

    Letting colonies somewhat grow, a bit more tolerance

--- tests/test_segmenting.py
+++ tests/test_segmenting.py
@@ -7,22 +7,26 @@ from generate_data import generate_data,colony_centres
 def test_segmenting():
 	times = 10
 	dimensions = (12,8)
-	colony_sizes = np.random.uniform(10,20,(times,*dimensions))
+	colony_sizes = np.dstack(
+			np.random.uniform(i+1,4*(i+1),dimensions)
+			for i in range(times)
+		).transpose(2,0,1)
 	resolution = (900,600)
 	bg = np.random.randint(0,256,3)
 	fg = np.random.randint(0,256,3)
 	
 	some_array = ColonyArray(
 			images = generate_data(colony_sizes,resolution,bg,fg),
-			dimensions = dimensions
+			dimensions = dimensions,
+		#	bg = bg,
 		)
 	
 	centres_control = colony_centres(dimensions,resolution)
-	assert_allclose(some_array.centres,centres_control,atol=1)
+	assert_allclose(some_array.centres,centres_control,atol=3)
 	
 	for i in (0,1):
 		borders_control = np.linspace(0,resolution[i],dimensions[i]+1)
-		assert_allclose(some_array.borders[i],borders_control,atol=1)
+		assert_allclose(some_array.borders[i],borders_control,atol=3)
 	
 	# for i,j in [ (dimensions[0],0), (0,dimensions[1]) ]:
 	# 	with raises(IndexError):
