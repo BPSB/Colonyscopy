f001b678fa080eed7cb596556afdc7608366d053
Gerrit Ansmann <bla@example.com>
2018-07-30 17:58:25 +0200

    Including image output for test purposes.

--- colonyscopy/tools.py
+++ colonyscopy/tools.py
@@ -1,5 +1,8 @@
 from scipy.signal.windows import blackman
 import numpy as np
+from PIL import Image
+import time
+import psutil
 
 def smoothen(data,width):
 	kernel = blackman(width)
@@ -13,3 +16,10 @@ def color_distance(data1,data2):
 	assert data1.shape[-1]==3
 	return np.sum(np.abs(data1-data2),axis=-1)
 
+def show_image(array):
+	img = Image.fromarray(array,"RGB")
+	img.show()
+	time.sleep(3)
+	for proc in psutil.process_iter():
+		if proc.name() == "display":
+			proc.kill()
--- tests/test_segmenting.py
+++ tests/test_segmenting.py
@@ -1,32 +1,46 @@
-from pytest import raises, approx
+from pytest import raises, approx, mark
 import numpy as np
 from numpy.testing import assert_allclose
 from colonyscopy import Plate
 from generate_data import generate_data,colony_centres
+from colonyscopy.tools import show_image
 
-def test_segmenting():
-	times = 10
-	layout = (12,8)
-	colony_sizes = np.dstack(
-			np.random.uniform(i+1,4*(i+1),layout)
-			for i in range(times)
-		).transpose(2,0,1)
-	resolution = (900,600)
-	bg = np.random.randint(0,256,3)
-	fg = np.random.randint(0,256,3)
-	
+times = 10
+layout = (12,8)
+colony_sizes = np.dstack(
+		np.random.uniform(i+1,4*(i+1),layout)
+		for i in range(times)
+	).transpose(2,0,1)
+resolution = (900,600)
+bg,fg,rim_color = np.random.randint(0,256,(3,3))
+rim_width = 5
+
+@mark.parametrize(
+		"noise_amp, tol",[
+		(    0    ,  3 ),
+		(   30    ,  3 ),
+	])
+def test_segmenting(noise_amp,tol):
 	some_array = Plate(
-			images = generate_data(colony_sizes,resolution,bg,fg),
+			images = generate_data(
+					colony_sizes,resolution,
+					bg,fg,
+					noise_amp,
+					rim_width,rim_color
+				),
 			layout = layout,
 		#	bg = bg,
 		)
+	
+	# for image in some_array.images:
+	# 	show_image(image)
 	
 	centres_control = colony_centres(layout,resolution)
 	assert_allclose(some_array.centres,centres_control,atol=3)
 	
 	for i in (0,1):
 		borders_control = np.linspace(0,resolution[i],layout[i]+1)
-		assert_allclose(some_array.borders[i],borders_control,atol=3)
+		assert_allclose(some_array.borders[i],borders_control,atol=tol)
 	
 	# for i,j in [ (layout[0],0), (0,layout[1]) ]:
 	# 	with raises(IndexError):
