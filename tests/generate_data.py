fe4f22b9a29a3162304a9a205cf9afdc30a23bff
Gerrit Ansmann <bla@example.com>
2018-07-30 17:57:46 +0200

    Including background image

--- tests/generate_data.py
+++ tests/generate_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 from itertools import product
+from pytest import approx, mark
 
 def midpoints(number,length):
 	"""
@@ -16,7 +17,12 @@ def colony_centres(layout,resolution):
 			midpoints(layout[i],resolution[i]) for i in (0,1)
 		],indexing="ij"))
 
-def generate_data(colony_sizes,resolution,bg=(0,0,0),fg=(255,255,255)):
+def generate_data(
+			colony_sizes, resolution,
+			bg=(0,0,0), fg=(255,255,255),
+			noise_amplitude = 0,
+			rim_width=0, rim_color=(255,255,255)
+		):
 	"""
 	Genererates artefical datasets for testing purposes.
 	
@@ -30,6 +36,14 @@ def generate_data(colony_sizes,resolution,bg=(0,0,0),fg=(255,255,255)):
 	
 	bg, fg: tuples of three integer
 		The colours for the background and colonies, respectively.
+		bg can also be an image, which is then used as a background for each generated image.
+	
+	rim_width, rim_color:
+		Width and color of a rim surrounding each colony.
+	
+	noise_amplitude:
+		Amplitude of normally distributed noise added to each pixel.
+		This does not account for over- and underflows, but the methods have to cope with single bright or dark pixels anyway.
 	"""
 	
 	bg = np.asarray(bg,dtype=np.uint8)
@@ -40,25 +54,47 @@ def generate_data(colony_sizes,resolution,bg=(0,0,0),fg=(255,255,255)):
 	positions = colony_centres(layout,resolution)
 	coordinates = np.indices(resolution).transpose(1,2,0)
 	
-	result = np.tile(bg,(times,*resolution,1))
+	if bg.shape==(3,):
+		result = np.tile(bg,(times,*resolution,1))
+	else:
+		result = np.tile(bg,(times,1,1,1))
+
 	# This loop could be made more concise with NumPy indexing, but it needs less memory.
 	for i in range(layout[0]):
 		for j in range(layout[1]):
 			distances = np.linalg.norm( coordinates-positions[i,j], axis=2 )
 			mask = distances<colony_sizes[:,None,None,i,j]
 			result[mask] = fg
+			if rim_width:
+				rim_mask = np.logical_and(
+						colony_sizes[:,None,None,i,j]-rim_width/2<distances,
+						colony_sizes[:,None,None,i,j]+rim_width/2>distances
+					)
+				result[rim_mask] = rim_color
+	
+	if noise_amplitude:
+		for time in range(times):
+			noise = np.random.normal(0,noise_amplitude,(*resolution,3))
+			result[time] += noise.astype(np.uint8)
 	
 	return result
 
-def test_generate_data():
+cases = mark.parametrize("homogoneous_bg",[False,True])
+
+@cases
+def test_generate_data(homogoneous_bg):
 	times = 4
 	layout = (5,7)
 	colony_sizes = np.random.uniform(3,10,(times,*layout))
 	resolution = (200,100)
-	bg = np.random.randint(0,256,3)
-	fg = np.random.randint(0,256,3)
+	fg,rim_color = np.random.randint(0,256,(2,3))
+	if homogoneous_bg:
+		bg = np.random.randint(0,256,3)
+	else:
+		bg = np.random.randint(0,256,(*resolution,3))
+	rim_width = 3
 	
-	data = generate_data(colony_sizes,resolution,bg,fg)
+	data = generate_data(colony_sizes,resolution,bg,fg,0,rim_width,rim_color)
 	
 	centres = colony_centres(layout,resolution)
 	for _ in range(1000):
@@ -68,5 +104,39 @@ def test_generate_data():
 				np.linalg.norm(pos-centres[i,j]) < colony_sizes[time,i,j]
 				for i,j in product(*map(range,layout))
 			)
-		assert np.all(data[(time,*pos)] == (fg if is_colony else bg))
+		is_rim = any(
+				colony_sizes[time,i,j]-rim_width < np.linalg.norm(pos-centres[i,j]) < colony_sizes[time,i,j]+rim_width
+				for i,j in product(*map(range,layout))
+			)
+		if is_rim:
+			np.all(data[(time,*pos)] == rim_color)
+		elif is_colony:
+			np.all(data[(time,*pos)] == fg)
+		else:
+			if homogoneous_bg:
+				np.all(data[(time,*pos)] == bg)
+			else:
+				np.all(data[(time,*pos)] == bg[pos])
+
+@cases
+def test_noise(homogoneous_bg):
+	times = 4
+	layout = (5,7)
+	colony_sizes = np.random.uniform(3,10,(times,*layout))
+	resolution = (200,100)
+	fg,rim_color = np.random.randint(0,256,(2,3))
+	if homogoneous_bg:
+		bg = np.random.randint(0,256,3)
+	else:
+		bg = np.random.randint(0,256,(*resolution,3))
+	rim_width = 3
+	noise_amplitude = 5
+	
+	data = [
+			generate_data(colony_sizes,resolution,bg,fg,noise,rim_width,rim_color)
+			for noise in (0,noise_amplitude)
+		]
+	
+	amplitude_estimate = np.std((data[0]-data[1]).astype(np.int8))
+	assert amplitude_estimate==approx(noise_amplitude,abs=1)
 
