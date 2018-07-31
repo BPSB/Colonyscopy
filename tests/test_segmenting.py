8abfb14e198b75aade120d97920c11d7236c17b4
Gerrit Ansmann <bla@example.com>
2018-07-31 09:03:28 +0200

    Test with background images.

--- tests/test_segmenting.py
+++ tests/test_segmenting.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 from colonyscopy import Plate
 from generate_data import generate_data,colony_centres
-from colonyscopy.tools import show_image
+from colonyscopy.tools import show_image, smoothen_image
 
 times = 10
 layout = (12,8)
@@ -14,18 +14,27 @@ colony_sizes = np.dstack(
 resolution = (900,600)
 bg,fg,rim_color = np.random.randint(0,256,(3,3))
 rim_width = 5
+noise_width = 20
+
+bg_img = np.empty((*resolution,3),dtype=np.uint8)
+for c in range(3):
+	middle = bg[c]//2+fg[c]//2
+	interval = sorted((bg[c],middle))
+	bg_img[:,:,c] = np.random.randint(*interval,resolution)
+bg_img = smoothen_image(bg_img,20)
 
 @mark.parametrize(
 		"noise_amp, tol",[
 		(    0    ,  3 ),
 		(   30    ,  3 ),
 	])
-def test_segmenting(noise_amp,tol):
+@mark.parametrize("background",[bg,bg_img])
+def test_segmenting(noise_amp,tol,background):
 	some_array = Plate(
 			images = generate_data(
 					colony_sizes,resolution,
-					bg,fg,
-					noise_amp,
+					background, fg,
+					noise_amp, noise_width,
 					rim_width,rim_color
 				),
 			layout = layout,
