2ed1c96625039d3e339e1307f1fd0a1c67bc9a01
Gerrit Ansmann <bla@example.com>
2019-02-06 14:57:38 +0100

    Test expanded mask

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,5 +1,6 @@
-from colonyscopy.tools import radial_profile
+from colonyscopy.tools import radial_profile, expand_mask
 import numpy as np
+from numpy.testing import assert_array_equal
 
 resolution = (200,200)
 centre = np.array([100,100])
@@ -18,4 +19,21 @@ class TestRadialProfile(object):
 		bins,values = radial_profile(gaußian,centre=centre)
 		expected = function(bins)
 		assert np.sum((values-expected)**2) < 2.0
+
+class TestExpandMask(object):
+	def test_zero_radius(self):
+		random_mask = np.random.choice([True,False],size=(100,80))
+		assert_array_equal( expand_mask(random_mask,0), random_mask )
+	
+	def test_single_point(self):
+		size = (100,100)
+		point = (50,50)
+		radius = 13
+		mask = np.zeros(size,dtype=bool)
+		mask[point] = True
+		expanded_mask = expand_mask(mask,radius)
+		for i in range(size[0]):
+			for j in range(size[1]):
+				dist = np.linalg.norm([point[0]-i,point[1]-j])
+				assert (dist<=radius) == expanded_mask[i,j]
 
