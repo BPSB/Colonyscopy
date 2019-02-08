ff298b8a53bf08913b33933a230972987b011f67
Gerrit Ansmann <bla@example.com>
2019-02-08 16:18:48 +0100

    Removed restriction to three colours.

--- colonyscopy/tools.py
+++ colonyscopy/tools.py
@@ -33,8 +33,6 @@ def color_distance(data1,data2):
 	"""
 	Determines the L1 norm between the two input arrays in color space. The color axis must be the last axis.
 	"""
-	assert data1.shape[-1]==3
-	assert data2.shape[-1]==3
 	return np.sum(np.abs(np.subtract(data1,data2,dtype=float)),axis=-1)
 
 class TestColorDistance(object):
